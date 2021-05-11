8bec58ab53515854da49d11c18024a014dffce78
Johnnatan <johnnatan.santos@juit.io>
2021-05-10 22:03:33 -0300

    feat: creating user and login working

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import UserView
+from .views import LoginView, UserView
 
-urlpatterns = [path("accounts/", UserView.as_view())]
+urlpatterns = [
+    path("accounts/", UserView.as_view()),
+    path("login/", LoginView.as_view()),
+]
