b11212872589fd2a829a4a6e63d5d1a46d38a0ce
Johnnatan <johnnatan.santos@juit.io>
2021-05-11 19:03:07 -0300

    feat: starting tests

--- activities/permissions.py
+++ activities/permissions.py
@@ -1,18 +1,22 @@
 from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import ValidationError
+from rest_framework import status
 
 
 class CreateActivityPermission(BasePermission):
     def has_permission(self, request, view):
-        if (
-            request.method == "POST"
-            or request.method == "GET"
-            and not request.user.is_superuser
-            and not request.user.is_staff
-        ):
+        if request.method == 'POST' and not request.user.is_superuser and not request.user.is_staff:
             return True
-        if (
-            request.method == "PUT"
-            and request.user.is_superuser
-            or request.user.is_staff
-        ):
+        
+        if request.method == 'POST' and request.user.is_superuser or request.user.is_staff:
+            validation_error = ValidationError(detail='You dont have permissions')
+            validation_error.status_code = status.HTTP_401_UNAUTHORIZED
+            raise validation_error
+        
+        if request.method == 'PUT' and request.user.is_superuser or request.user.is_staff:
             return True
+        
+        if request.method == 'GET':
+            return True
+
+        return False
